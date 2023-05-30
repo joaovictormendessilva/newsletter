e8996f856832dc6729c5506b9ec1c3cf8c74cbea
João Victor <joao_victor-08@hotmail.com>
2023-05-30 19:32:32 -0300

    Making responsive

--- src/components/Illustration/Illustration.tsx
+++ src/components/Illustration/Illustration.tsx
@@ -3,12 +3,18 @@ import styles from './Illustration.module.css'
 
 // Images
 import desktopIllustration from '../../assets/img/illustration-desktop.svg'
+import mobileIllustration from '../../assets/img/illustration-mobile.svg'
 
 export function Illustration(){
     return (
         <div className={styles.illustration}>
             <img
+                className={styles.desktopIllustration}
                 src={desktopIllustration}
+            />
+            <img
+                className={styles.mobileIllustration}
+                src={mobileIllustration}
             />
         </div>
     )
